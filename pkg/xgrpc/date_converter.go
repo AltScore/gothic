2a0db2b3e2d42febcf3740e4453ed809b1047cf2
Charly Fau <carlos.fau@altscore.ai>
2023-03-29 12:27:39 -0300

    Add date conversion to proto

---
+++ pkg/xgrpc/date_converter.go
@@ -0,0 +1,19 @@
+package xgrpc
+
+import (
+	xdate "github.com/AltScore/gothic/pkg/xtime/date"
+	"google.golang.org/genproto/googleapis/type/date"
+)
+
+// DateToProto converts a gothic date.Date to a google proto date.Date
+func DateToProto(d xdate.Date) *date.Date {
+	if d.IsZero() {
+		return nil
+	}
+
+	return &date.Date{
+		Year:  int32(d.Year()),
+		Month: int32(d.Month()),
+		Day:   int32(d.Day()),
+	}
+}
---
+++ pkg/xgrpc/date_converter_test.go
@@ -0,0 +1,34 @@
+package xgrpc
+
+import (
+	xdate "github.com/AltScore/gothic/pkg/xtime/date"
+	"google.golang.org/genproto/googleapis/type/date"
+	"reflect"
+	"testing"
+)
+
+func TestDateToProto(t *testing.T) {
+	tests := []struct {
+		name string
+		args xdate.Date
+		want *date.Date
+	}{
+		{
+			name: "non empty date",
+			args: xdate.New(2019, 11, 29),
+			want: &date.Date{Year: 2019, Month: 11, Day: 29},
+		},
+		{
+			name: "empty date",
+			args: xdate.Date{},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := DateToProto(tt.args); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("DateToProto() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
