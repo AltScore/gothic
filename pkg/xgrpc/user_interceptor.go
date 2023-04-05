36ee890b35911370ec51a9d9d335fa2b1fad975e
Ignacio Loyola <ignacio.loyola@altscore.ai>
2023-04-05 14:01:13 -0400

    wip

--- pkg/xgrpc/user_interceptor.go
+++ pkg/xgrpc/user_interceptor.go
@@ -4,16 +4,19 @@ import (
 	"context"
 	"github.com/AltScore/auth-api/lib/auth"
 	"github.com/AltScore/auth-api/lib/jwt"
+	echojwt "github.com/golang-jwt/jwt/v4"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 )
+
+const JwtMetadataKey = "authorization"
 
 func NewUserServerInterceptor(jwtManager jwt.Manager, next grpc.UnaryServerInterceptor) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 		md, found := metadata.FromIncomingContext(ctx)
 
 		if found {
-			if jwtStr, ok := md["jwt"]; ok && len(jwtStr) > 0 {
+			if jwtStr, ok := md[JwtMetadataKey]; ok && len(jwtStr) > 0 {
 
 				user, err := jwtManager.UserFromJwt(jwtStr[0])
 
@@ -27,5 +30,24 @@ func NewUserServerInterceptor(jwtManager jwt.Manager, next grpc.UnaryServerInter
 		}
 
 		return next(ctx, req, info, handler)
+	}
+}
+
+// NewUserClientInterceptor is a gRPC client-side interceptor that adds the user credentials to the output request context.
+func NewUserClientInterceptor() grpc.UnaryClientInterceptor {
+	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		// Take user from context
+		tokenObj := ctx.Value(auth.JwtKey)
+
+		token, ok := tokenObj.(*echojwt.Token)
+
+		if ok {
+			// Add token to metadata
+			md := metadata.Pairs(JwtMetadataKey, token.Raw)
+			ctx = metadata.NewOutgoingContext(ctx, md)
+		}
+
+		// call invoker
+		return invoker(ctx, method, req, reply, cc, opts...)
 	}
 }
