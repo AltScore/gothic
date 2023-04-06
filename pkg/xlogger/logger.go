61abb9e7b484b828f070294b54d39d63b4df217e
Charly Fau <carlos.fau@altscore.ai>
2023-04-06 13:36:14 -0300

    Deprecate logger Remove interceptors (were moved to auth-api)

--- pkg/xlogger/logger.go
+++ pkg/xlogger/logger.go
@@ -2,6 +2,8 @@ package xlogger
 
 import "go.uber.org/zap"
 
+// Logger is a logger that can be used to log messages.
+// Deprecated: use https://pkg.go.dev/go.uber.org/zap@v1.24.0/zap instead.
 type Logger interface {
 	Debug(msg string, fields ...zap.Field)
 	Info(msg string, fields ...zap.Field)
