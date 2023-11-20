3597a3d911fb50715a4c14c074f1d6848b994cda
Charly Fau <carlos.fau@altscore.ai>
2023-11-20 12:30:06 -0300

    Add convert error

---
+++ pkg/xeh/convert_error.go
@@ -0,0 +1,6 @@
+package xeh
+
+// ConvertError convert Event Horizon errors to standard format
+func ConvertError(err error, entity string, keyFmt string, args ...interface{}) error {
+	return err
+}
