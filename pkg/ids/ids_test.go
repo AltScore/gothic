e5d32be714405e83f9e7210ad857d93111218b77
Charly Fau <carlos.fau@altscore.ai>
2023-12-12 20:28:51 -0300

    Add test

--- pkg/ids/ids_test.go
+++ pkg/ids/ids_test.go
@@ -1,6 +1,9 @@
 package ids
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 const (
 	equal       = 0
@@ -63,6 +66,42 @@ func TestCompare(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Compare(tt.args.id, tt.args.other); got != tt.want {
 				t.Errorf("Compare() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestOrDefault(t *testing.T) {
+	type args struct {
+		id        Id
+		defaultId Id
+	}
+	tests := []struct {
+		name string
+		args args
+		want Id
+	}{
+		{
+			name: "default id is returned when id is empty",
+			args: args{
+				id:        Empty(),
+				defaultId: MustParse("7fc154a2-84a1-4a9f-a387-48de083a565e"),
+			},
+			want: MustParse("7fc154a2-84a1-4a9f-a387-48de083a565e"),
+		},
+		{
+			name: "id is returned when id is not empty",
+			args: args{
+				id:        MustParse("d7eaba62-fd6e-412c-8707-9a71b9c5e7ec"),
+				defaultId: MustParse("44ecbe0f-73e0-422b-a590-69985900dff6"),
+			},
+			want: MustParse("d7eaba62-fd6e-412c-8707-9a71b9c5e7ec"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := OrDefault(tt.args.id, tt.args.defaultId); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("OrDefault() = %v, want %v", got, tt.want)
 			}
 		})
 	}
